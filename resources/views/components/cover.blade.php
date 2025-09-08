9ab823bccb0233eb7ac5c773685e4f1a79d518eb
mac <fajrudinsauqi@gmail.com>
2025-09-08 22:38:23 +0700

    feat; add bg to cover

--- resources/views/components/cover.blade.php
+++ resources/views/components/cover.blade.php
@@ -4,7 +4,7 @@
             <div>
                 <img src="{{ asset('images/logo um.png') }}" alt="" style="width: 48px; height: 48px;">
             </div>
-            <h3 class="text-center mx-2 fw-bold mt-5 h5 text-white">E-Module “Plantelligence” Cerdas
+            <h3 class="text-center fw-bold mt-5 h5 text-white bg-gray px-2 py-3 rounded-md">E-Module “Plantelligence” Cerdas
                 Belajar Fisiologi Tumbuhan dengan PBL dan AI</h3>
         </div>
         <div class="mt-1 mx-5 text-center text-white" style="position: absolute; bottom: 0; left: 0; right: 0; margin-bottom: 64px;">
